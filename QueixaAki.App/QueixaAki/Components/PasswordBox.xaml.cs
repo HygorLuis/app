5468efec985c4419e27bbe98ac527153b86924bd
HygorAgger <hygor@agger.com.br>
2020-12-08 15:13:55 -0300

    PasswordBox

---
+++ QueixaAki.App/QueixaAki/Components/PasswordBox.xaml.cs
@@ -0,0 +1,52 @@
+﻿using QueixaAki.ViewModels.Components;
+using Xamarin.Forms;
+
+namespace QueixaAki.Components
+{
+    public partial class PasswordBox
+    {
+        private PasswordBoxViewModel _viewModel;
+
+        public PasswordBox()
+        {
+            InitializeComponent();
+
+            _viewModel = new PasswordBoxViewModel();
+            BindingContext = _viewModel;
+        }
+
+        public string Text { get; set; }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(PasswordBox),
+            "",
+            BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (newValue != null && bindable is PasswordBox control)
+                {
+                    var actualNewValue = (string)newValue;
+                    control.BaseEntryBox.Text = actualNewValue;
+                }
+            });
+
+        public string Placeholder { get; set; }
+
+        public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(
+            nameof(Placeholder),
+            typeof(string),
+            typeof(PasswordBox),
+            "",
+            BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (newValue != null && bindable is PasswordBox control)
+                {
+                    var actualNewValue = (string)newValue;
+                    control.BaseEntryBox.Placeholder = actualNewValue;
+                }
+            });
+    }
+}
---
+++ QueixaAki.App/QueixaAki/ViewModels/Components/PasswordBoxViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+using QueixaAki.ViewModels.Base;
+using Xamarin.Forms;
+
+namespace QueixaAki.ViewModels.Components
+{
+    public class PasswordBoxViewModel : BaseViewModel
+    {
+        public PasswordBoxViewModel()
+        {
+            ShowHideTapCommand = new Command(() =>
+            {
+                IsPasswordShow = !IsPasswordShow;
+            });
+        }
+
+        public ICommand ShowHideTapCommand { get; set; }
+
+        private bool _isPasswordShow = true;
+        public bool IsPasswordShow
+        {
+            get { return _isPasswordShow; }
+            set
+            {
+                _isPasswordShow = value;
+                OnPropertyChanged();
+                OnPropertyChanged("ShowHideIcon");
+            }
+        }
+
+        public string ShowHideIcon => IsPasswordShow ? "eye.png" : "hide.png";
+    }
+}
