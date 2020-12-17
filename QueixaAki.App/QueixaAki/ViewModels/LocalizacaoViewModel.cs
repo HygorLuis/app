cd3abcbce85cf805c215a58b237833a9cfdf060f
HygorAgger <hygor@agger.com.br>
2020-12-17 00:38:04 -0300

    Atualizar endereço do Pin

---
+++ QueixaAki.App/QueixaAki/ViewModels/LocalizacaoViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using QueixaAki.ViewModels.Base;
+using Xamarin.Essentials;
+
+namespace QueixaAki.ViewModels
+{
+    public class LocalizacaoViewModel : BaseViewModel
+    {
+        public LocalizacaoViewModel()
+        {
+
+        }
+
+        public async Task<string> GetEndereço(double latitude, double longitude)
+        {
+            var addrs = (await Geocoding.GetPlacemarksAsync(new Location(latitude, longitude))).FirstOrDefault();
+            if (addrs == null) return "";
+
+            var endereco = $"{addrs.Thoroughfare}, {addrs.SubThoroughfare}";
+            var bairro = addrs.SubLocality;
+            var cidade = $"{addrs.SubAdminArea} - {Estados[addrs.AdminArea]}";
+            var cep = addrs.PostalCode;
+            var pais = addrs.CountryName;
+
+            return $"{endereco} - {bairro}, {cidade}";
+        }
+
+        public async Task<Location> GetLocalizacao()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+            var cts = new CancellationTokenSource();
+            return await Geolocation.GetLocationAsync(request, cts.Token);
+        }
+
+        private static IReadOnlyDictionary<string, string> Estados = new Dictionary<string, string>
+        {
+            {"Acre", "AC"},
+            {"Alagoas", "AL"},
+            {"Amapá", "AP"},
+            {"Amazonas", "AM"},
+            {"Bahia", "BA"},
+            {"Ceara", "CE"},
+            {"Distrito Federal", "DF"},
+            {"Espirito Santo", "ES"},
+            {"Goiás", "GO"},
+            {"Maranhão", "MA"},
+            {"Mato Grosso", "MT"},
+            {"Mato Grosso do Sul", "MS"},
+            {"Minas Gerais", "MG"},
+            {"Para", "PA"},
+            {"Paraíba", "PB"},
+            {"Paraná", "PR"},
+            {"Pernambuco" , "PE"},
+            {"Piauí", "PI"},
+            {"Rio de Janeiro", "RJ"},
+            {"Rio Grande do Norte", "RN"},
+            {"Rio Grande do Sul", "RS"},
+            {"Rondônia", "RO"},
+            {"Roraima", "RR"},
+            {"Santa Catarina", "SC"},
+            {"São Paulo", "SP"},
+            {"Sergipe", "SE"},
+            {"Tocantins", "TO"},
+        };
+    }
+}
--- QueixaAki.App/QueixaAki/Views/LocalizacaoView.xaml.cs
+++ QueixaAki.App/QueixaAki/Views/LocalizacaoView.xaml.cs
@@ -1,16 +1,27 @@
 ﻿using System;
-using System.Linq;
-using System.Threading;
-using Xamarin.Essentials;
+using System.Threading.Tasks;
+using QueixaAki.ViewModels;
 using Xamarin.Forms.GoogleMaps;
 
 namespace QueixaAki.Views
 {
     public partial class LocalizacaoView
     {
+        private LocalizacaoViewModel _viewModel;
+
+        private Pin pinAtual = new Pin
+        {
+            Type = PinType.Place,
+            Label = "Sua queixa está aki?",
+            IsDraggable = true
+        };
+
         public LocalizacaoView()
         {
             InitializeComponent();
+
+            _viewModel = new LocalizacaoViewModel();
+            BindingContext = _viewModel;
 
             LocalizacaoAtual();
         }
@@ -19,61 +30,54 @@ namespace QueixaAki.Views
         {
             try
             {
-                var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
-                var cts = new CancellationTokenSource();
-                var locationActual = await Geolocation.GetLocationAsync(request, cts.Token);
+                var localizacao = await _viewModel.GetLocalizacao();
 
                 Map.MyLocationEnabled = true;
                 Map.UiSettings.MyLocationButtonEnabled = true;
 
-                var pinAtual = new Pin
-                {
-                    Type = PinType.Place,
-                    Label = "Sua queixa está aki?",
-                    Position = new Position(locationActual.Latitude, locationActual.Longitude),
-                    IsDraggable = true
-                };
-
-                Map.Pins.Add(pinAtual);
-                Map.SelectedPin = pinAtual;
-                Map.MoveToRegion(MapSpan.FromCenterAndRadius(pinAtual.Position, Distance.FromKilometers(0.5)));
-
-
-                var addrs = (await Geocoding.GetPlacemarksAsync(new Location(locationActual.Latitude, locationActual.Longitude))).FirstOrDefault();
-                var Street = $"{addrs.Thoroughfare} {addrs.SubThoroughfare}";
-                var City = $"{addrs.PostalCode} {addrs.Locality}";
-                var Country = addrs.CountryName;
+                await SelectedPin(localizacao.Latitude, localizacao.Longitude);
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
-
+                // ignored
             }
         }
 
-
-
-        /*public async void btn_clicked(object sender, System.EventArgs e)
+        private async void Map_OnPinDragEnd(object sender, PinDragEventArgs e)
         {
             try
             {
-                var request = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
-                var cts = new CancellationTokenSource();
-                var locationActual = await Geolocation.GetLocationAsync(request, cts.Token);
-    
-                var location = new Location(locationActual.Latitude, locationActual.Longitude);
-                var options = new MapLaunchOptions
-                {
-                    Name = EntryNome.Text
-                };
-                await Map.OpenAsync(location, options);
+                if (e == null || e.Pin == null) return;
+
+                await SelectedPin(e.Pin.Position.Latitude, e.Pin.Position.Longitude);
             }
             catch (Exception ex)
             {
-                // Não foi possivel acessar o mapa
-                await DisplayAlert("Erro : ", ex.Message, "Ok");
+                // ignored
             }
-        }*/
+        }
+
+        private async Task SelectedPin(double latitude, double longitude)
+        {
+            try
+            {
+                pinAtual.Position = new Position(latitude, longitude);
+                pinAtual.Address = await _viewModel.GetEndereço(latitude, longitude);
+
+                if (Map.Pins.Count == 0)
+                    Map.Pins.Add(pinAtual);
+                else
+                    Map.Pins[0] = pinAtual;
+
+                Map.SelectedPin = pinAtual;
+                Map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(latitude, longitude), Distance.FromKilometers(0.5)));
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+        }
+
         private void Button_OnClicked(object sender, EventArgs e)
         {
             var teste = Map.SelectedPin;
